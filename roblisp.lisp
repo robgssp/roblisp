8fd0693343d4e5fce04db255e876a28e4bc1efed
Robert Glossop <robgssp@gmail.com>
2014-03-11 23:45:57 -0400

    Added lexically scoped lambdas

--- roblisp.lisp
+++ roblisp.lisp
@@ -1,4 +1,8 @@
-;; A lazy lisp
+; A lazy lisp
+
+(declaim (optimize (speed 0) (debug 3)))
+
+;; util
 
 (defmacro thunk (&body body)
   `(lambda () ,@body))
@@ -12,47 +16,54 @@
 (defun force (f)
   (funcall f))
 
+(defun take (n lst)
+  (if (= n 0)
+      nil
+      (cons (car lst) (take (1- n) (cdr lst)))))
+
+;; impl
+
 (defun rl-eval (exp env)
   (thunk
-    (cond ((symbolp exp) (lookup exp env))
+    (cond ((symbolp exp) (force (lookup exp env)))
           ((or (numberp exp) (stringp exp)) exp)
           ((consp exp)
            (case (car exp)
              ((quote) (cadr exp))
-             (t (apply #'rl-apply
-                       (rl-eval (car exp) env)
-                       (mapcar (lambda (ex1) (rl-eval ex1 env)) (cdr exp)))))))))
+             ((lambda) `(&proc ,env ,(cadr exp) ,(caddr exp)))
+             (t (let ((exp1 (normalize-call exp)))
+                  (rl-apply (rl-eval (car exp1) env)
+                            (rl-eval (cadr exp1) env)))))))))
 
 (defun lookup (var env)
-  (if (null env)
-      (error "No such variable")
-      (let ((res (lookup1 var (car env))))
-        (if (consp res)
-            (cdr res)
-            (lookup var (cdr env))))))
+  (cond ((null env)
+         (error "No such variable"))
+        ((eq (caar env) var)
+         (cdar env))
+        (t (lookup var (cdr env)))))
 
-(defun lookup1 (var env)
-  (cond ((null env) nil)
-        ((eq var (caar env)) (car env))
-        (t (lookup1 var (cdr env)))))
+(defun bind (var exp env)
+    (cons (cons var exp) env))
 
-(defun rl-apply (f &rest xs)
-  (if (null xs)
-      (force f)
-      (apply #'rl-apply
-             (thunk (funcall (force f) (car xs)))
-             (cdr xs))))
+(defun rl-apply (f x)
+  (let ((f (force f)))
+    (if (functionp f)
+        (funcall f x)
+        (force
+         (rl-eval (cadddr f)
+                  (bind (caddr f) x (cadr f)))))))
 
-;; (defun rl-apply (f x)
-;;   (funcall (force f) x))
+(defun normalize-call (fc)
+  (reduce (lambda (x acc) (list x acc)) (cddr fc) :initial-value (take 2 fc)))
 
 (defparameter *default-env*
-  `(((t . t)
-     (nil . nil)
-     (+ . ,(lambda (x) (lambda (y) (+ (force x) (force y)))))
-     (if . ,(lambdas (test tr fl)
-                     (if (force test)
-                         (force tr)
-                         (force fl))))
-     (error . ,(lambda (x) (error (force x)))))))
+  (mapcar (lambda (k) (cons (car k) (thunk (cdr k))))
+          `((t . t)
+            (nil . nil)
+            (+ . ,(lambda (x) (lambda (y) (+ (force x) (force y)))))
+            (if . ,(lambdas (test tr fl)
+                            (if (force test)
+                                (force tr)
+                                (force fl))))
+            (error . ,(lambda (x) (error (force x)))))))
 
